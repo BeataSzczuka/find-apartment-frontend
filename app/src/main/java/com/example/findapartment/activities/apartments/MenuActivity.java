807e2c7198711be56e2ab4f2a996bfc098bbf18e
BeataSzczuka <beata.szczuka@gmail.com>
2021-03-31 19:55:38 +0200

    Create user and apartment adding forms

--- app/src/main/java/com/example/findapartment/activities/apartments/MenuActivity.java
+++ app/src/main/java/com/example/findapartment/activities/apartments/MenuActivity.java
@@ -15,4 +15,17 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
     }
+
+    public void navigateToAddUser(View view) {
+        Intent i=new Intent(getBaseContext(),AddUserActivity.class);
+        startActivity(i);
+    }
+    public void navigateToAllApartments(View view) {
+        Intent i=new Intent(getBaseContext(), ApartmentListActivity.class);
+        startActivity(i);
+    }
+    public void navigateToAddApartment(View view) {
+        Intent i=new Intent(getBaseContext(), AddApartmentActivity.class);
+        startActivity(i);
+    }
 }
